6c9cdc3ff039365a11de67898d52e7b7a0a5f625
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-18 12:42:48 +0530

    Add files via upload

---
+++ conditions3.py
@@ -0,0 +1,12 @@
+age = int(input())
+
+if(age<18):
+
+    print("age is not eligible for driving")
+
+elif(age == 18):
+
+    print("we are not sure")
+
+else :
+    print("age is eligible for driving")

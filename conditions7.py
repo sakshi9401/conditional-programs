cede7956caa11161b8eecdd7201ce4a9943f5522
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-18 20:07:51 +0530

    Add files via upload

---
+++ conditions7.py
@@ -0,0 +1,16 @@
+#to find the smallest one among three
+a = int(input())
+b =int(input())
+c =int(input())
+
+if a<b and a<c :
+    
+    print("a is the smallest among 3")
+
+elif b<a and b<c :
+
+    print("b is the smallest among 3")
+
+else :
+
+    print("c is the smallest among 3")

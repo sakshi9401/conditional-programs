8eb8640e8078b84ddd394e12a03714dd5d8a5777
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-18 12:41:56 +0530

    Add files via upload

---
+++ conditions1.py
@@ -0,0 +1,29 @@
+a = 9
+
+b = 5
+
+if a>b :
+
+    print("a is greater")
+
+else :
+
+    print(" b is greater")
+
+#input from user
+a = int(input())
+
+
+b = int(input())
+
+if a>b :
+
+    print("a is greater")
+
+elif (a==b) :
+
+    print("a and b is equal")
+
+else :
+
+    print(" b is greater")

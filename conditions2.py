6e5404bde24c219fc6f215e746f3c1a734a68adb
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-18 12:42:27 +0530

    Add files via upload

---
+++ conditions2.py
@@ -0,0 +1,41 @@
+a = [1, 2, 3, 4]#list
+
+if 5 in a :
+
+    print("5 is in a")
+
+else :
+
+    print("5 is not in a")
+
+b = {"mango", "orange", "apple", "grapes"}#set
+
+if "apple" in b:
+
+    print("apple is in b")
+
+else:
+
+    print("apple is not in b")
+
+c = (9, 6 ,4, 2)#tuple
+
+if 6 in c:
+
+    print("6 is in c")
+
+else :
+
+    print("6 is not in c")
+
+d = {"year" : 2020,
+     "colour" : "red",
+     "model" : "ford"}#dictionary
+if "colour" in d:
+
+    print("colour is in d")
+
+else :
+
+    print("colour is not in d")
+
